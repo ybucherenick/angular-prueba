9462d0e1ed120032de90a378d0453961cd2c7532
ybucherenick <tomas.diaz06.p@gmail.com>
2019-05-22 20:02:07 -0400

    se agrega grafica

--- app/view/view.js
+++ app/view/view.js
@@ -1,50 +1,107 @@
 "use strict";
 
-(function() {
-    app.controller("ViewCtrl", [
+(function () {
+  app.controller("ViewCtrl", [
     "$scope",
     "$q",
     "$log",
-    "$rootScope",
     "$NodoClient",
-    function(
-        $scope,
-        $q,
-        $log,
-        $rootScope,
-        $NodoClient,
+    function (
+      $scope,
+      $q,
+      $log,
+      $NodoClient
     ) {
-      
+
       $scope.servidores = [];
       loadData();
 
       function loadData() {
-          // Obtengo todos los servidores
-          $NodoClient.getTodosServidores().then(
-              function(_resolves) {
-                $scope.servidores = _resolves.data.servidores;
-                // Busco las relaciones de cada servidor
-                $scope.servidores.forEach(function(item){
-                  $NodoClient.getRelacion(item.id).then(
-                    function(_resolves2){
-                      item.conectado = _resolves2.data.conectados;
-                    },
-                    function(_error) {
-                        $log.error(_error);
-                    }
-                  );
+        // var defered = $q.defer();
+        var chain = $q.when();
 
-                });
-                console.log(_resolves.data.servidores);
-              },
-              function(_error) {
-                  $log.error(_error);
-              }
-          );
+
+        // Obtengo todos los servidores
+        $NodoClient.getTodosServidores().then(
+          function (_resolves) {
+            $scope.servidores = _resolves.data.servidores;
+            // Busco las relaciones de cada servidor
+            for (const item of $scope.servidores) {
+              chain =  chain.then(function(){
+                  $NodoClient.getRelacion(item.id).then(
+                function(_resolves2){
+                  item.conectado = _resolves2.data.conectados;
+                },
+                function(_error) {
+                    $log.error(_error);
+                })
+              });
+            }
+
+            chain.then(function(){
+              setTimeout(() => {
+                mostrarGrafica();  
+              }, 100);
+            });
+          
+          },
+          function (_error) {
+            $log.error(_error);
+          }
+        );
+      }
+
+      function mostrarGrafica() {
+
+        // Themes begin
+        am4core.useTheme(am4themes_animated);
+        // Themes end
+
+        var chart = am4core.create("chartdiv", am4plugins_forceDirected.ForceDirectedTree);
+
+        var networkSeries = chart.series.push(new am4plugins_forceDirected.ForceDirectedSeries())
+        networkSeries.dataFields.linkWith = "conectado";
+        networkSeries.dataFields.id = "id";
+        networkSeries.dataFields.pais = "pais";
+        networkSeries.dataFields.grupo = "grupo";
+        networkSeries.dataFields.host = "host";
+        networkSeries.dataFields.servicio = "servicio";
+
+        networkSeries.nodes.template.label.text = "{id}";
+        networkSeries.fontSize = 10;
+
+        var nodeTemplate = networkSeries.nodes.template;
+        nodeTemplate.tooltipText = "{id}" + "\n" + "{pais}";
+        nodeTemplate.fillOpacity = 1;
+        nodeTemplate.label.hideOversized = false;
+        nodeTemplate.label.truncate = false;
+
+        var linkTemplate = networkSeries.links.template;
+        linkTemplate.strokeWidth = 1;
+        var linkHoverState = linkTemplate.states.create("hover");
+        linkHoverState.properties.strokeOpacity = 1;
+        linkHoverState.properties.strokeWidth = 2;
+
+        nodeTemplate.events.on("over", function (event) {
+          var dataItem = event.target.dataItem;
+          dataItem.childLinks.each(function (link) {
+            link.isHover = true;
+          })
+        })
+
+        nodeTemplate.events.on("out", function (event) {
+          var dataItem = event.target.dataItem;
+          dataItem.childLinks.each(function (link) {
+              link.isHover = false;
+          })
+        })
+
+
+        networkSeries.data = $scope.servidores;
 
       }
 
 
-        }
-    ]);
+    }
+  ]);
 }.call());
